1b7dc5eeac0c723d8df3fc5deed25f39e6c54708
Hariprasad Kasavajjala <31498795+hpkhariprasad@users.noreply.github.com>
2025-09-20 23:44:56 +0530

    Create FetchRequest.cs

---
+++ src/CostAdvisor.Shared/Models/FetchRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CostAdvisor.Shared.Models
+{
+    public class FetchRequest
+    {
+        public string AccountId { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+    }
+}
