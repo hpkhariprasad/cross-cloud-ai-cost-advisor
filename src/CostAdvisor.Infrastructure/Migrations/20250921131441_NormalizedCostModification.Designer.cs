f230ccc70682d32caac0f2a021688904d9515510
Hariprasad Kasavajjala <31498795+hpkhariprasad@users.noreply.github.com>
2025-09-21 18:45:25 +0530

    DB Migration

--- src/CostAdvisor.Infrastructure/Migrations/20250921131441_NormalizedCostModification.Designer.cs
+++ src/CostAdvisor.Infrastructure/Migrations/20250921131441_NormalizedCostModification.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CostAdvisor.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CostAdvisor.Infrastructure.Migrations
 {
     [DbContext(typeof(CostAdvisorDbContext))]
-    partial class CostAdvisorDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250921131441_NormalizedCostModification")]
+    partial class NormalizedCostModification
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/CostAdvisor.Infrastructure/Migrations/20250921131441_NormalizedCostModification.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CostAdvisor.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NormalizedCostModification : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cost",
+                table: "NormalizedCosts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Cost",
+                table: "NormalizedCosts",
+                type: "numeric(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
