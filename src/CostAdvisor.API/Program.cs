98b2561527e6eb3f6b264d6454a5504df850573f
Hariprasad Kasavajjala <31498795+hpkhariprasad@users.noreply.github.com>
2025-09-21 17:47:39 +0530

    DB Connection Add

--- src/CostAdvisor.API/Program.cs
+++ src/CostAdvisor.API/Program.cs
@@ -1,8 +1,12 @@
 using CostAdvisor.Core.Providers;
 using CostAdvisor.Core.Repositories;
 using CostAdvisor.Core.Services;
+using CostAdvisor.Infrastructure.Data;
 using CostAdvisor.Infrastructure.Providers;
 using CostAdvisor.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +19,17 @@ builder.Services.AddSingleton<ICloudBillingProvider, AWSBillingProviderDummy>();
 builder.Services.AddSingleton<ICloudBillingProvider, AzureBillingProviderDummy>();
 builder.Services.AddSingleton<ICloudBillingProvider, GCPBillingProviderDummy>();
 builder.Services.AddScoped<BillingService>();
+builder.Services.AddDbContext<CostAdvisorDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
+// Ensure database is created and migrations are applied
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<CostAdvisorDbContext>();
+    dbContext.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
