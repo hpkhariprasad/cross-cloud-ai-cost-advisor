f9a73ed172a96595758c38b585ece35ceee95fd3
Hariprasad Kasavajjala <31498795+hpkhariprasad@users.noreply.github.com>
2025-09-22 22:10:37 +0530

    Open AI integration

--- src/CostAdvisor.API/Program.cs
+++ src/CostAdvisor.API/Program.cs
@@ -6,6 +6,7 @@ using CostAdvisor.Infrastructure.Providers;
 using CostAdvisor.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using OpenAI;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,15 +14,28 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
 builder.Services.AddScoped<ICostRepository, CostRepository>();
 builder.Services.AddSingleton<ICloudBillingProvider, AWSBillingProviderDummy>();
 builder.Services.AddSingleton<ICloudBillingProvider, AzureBillingProviderDummy>();
 builder.Services.AddSingleton<ICloudBillingProvider, GCPBillingProviderDummy>();
+
+builder.Services.AddScoped<IRecommendationRepository, RecommendationRepository>();
+builder.Services.AddScoped<IRecommendationService, RecommendationService>(); 
 builder.Services.AddScoped<BillingService>();
 builder.Services.AddDbContext<CostAdvisorDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<OpenAIClient>(sp =>
+{
+    var config = sp.GetRequiredService<IConfiguration>();
+    var apiKey = config["OpenAI:ApiKey"];
+    return new OpenAIClient(apiKey);
+});
 var app = builder.Build();
 
 // Ensure database is created and migrations are applied
@@ -40,5 +54,6 @@ app.MapControllers();
 app.UseHttpsRedirection();
 
 app.Run();
+
 
 
